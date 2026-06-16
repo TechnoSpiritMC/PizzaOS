5627b33346e8d411cada44aa318df7b93b734bc9
technospirit <d.boryskin21@assomption.bzh>
2026-06-17 00:27:29 +0200

    Updated stdint.h to also include a size_t type.

--- TechnOSGrub/src/include/stdint.h
+++ TechnOSGrub/src/include/stdint.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define NULL 0
+
 typedef signed   char int8_t;
 typedef unsigned char uint8_t;
 
@@ -8,6 +10,7 @@ typedef unsigned short uint16_t;
 
 typedef signed   long int int32_t;
 typedef unsigned long int uint32_t;
+typedef unsigned long int size_t;
 
 typedef signed   long long int int64_t;
 typedef unsigned long long int uint64_t;
