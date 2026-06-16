9487c5620ba4e441cfb809647b08d450864c32c1
technospirit <d.boryskin21@assomption.bzh>
2026-06-17 00:28:07 +0200

    Updated string.h to include fixed-length string comparison.

--- TechnOSGrub/src/stdlib/string.h
+++ TechnOSGrub/src/stdlib/string.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <stdbool.h>
-#include <stdint.h>
+#include "../include/stdint.h"
 
 typedef char* string_t;
 
@@ -12,4 +12,11 @@ inline bool strcmp(const char* str1, const char* str2) {
         idx++;
     }
     return str1[idx] == '\0' && str2[idx] == '\0';
+}
+
+inline bool strcmpFixed(const char* str1, const char* str2, uint32_t len) {
+    for (int idx = 0; idx < len; idx++) {
+        if (str1[idx] != str2[idx]) return false;
+    }
+    return true;
 }
