ab796545e0f711ab4e082f045a8fa3c3be816700
technospirit <d.boryskin21@assomption.bzh>
2026-06-19 23:57:32 +0200

    Started implementing app logic with an App interface.

---
+++ TechnOSGrub/src/apps/IApplication.cpp
@@ -0,0 +1,6 @@
+//
+// Created by denis on 6/19/2026.
+//
+
+#include "include/IApplication.h"
+
---
+++ TechnOSGrub/src/apps/include/IApplication.h
@@ -0,0 +1,8 @@
+//
+// Created by denis on 6/19/2026.
+//
+
+#ifndef ASM_IAPPLICATION_H
+#define ASM_IAPPLICATION_H
+
+#endif //ASM_IAPPLICATION_H
